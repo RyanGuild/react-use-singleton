882901be41c8a70294ad761236aaec2f082bb6c8
Ryan Guild <ryanwork489@gmail.com>
2020-05-15 11:09:46 -0400

    changed types

--- useSingleton.d.ts
+++ useSingleton.d.ts
@@ -1,14 +1,18 @@
 import * as React from "react";
-interface Constructor<T> {
-    name: string;
-    new (): T;
+export interface Constructor<T> {
+  name: string;
+  new (): T;
 }
-interface Singleton {
-    untrackedProperies?: string[];
-    constructor: Constructor<any>;
+export interface Singleton {
+  untrackedProperies?: string[];
+  constructor: Constructor<any>;
 }
-export declare function SingletonProvider(props: React.PropsWithChildren<{
+export declare function SingletonProvider(
+  props: React.PropsWithChildren<{
     singleton: Singleton;
-}>): JSX.Element;
-export declare function useSingleton<S extends Singleton>(singletonClass: Constructor<S>): S;
+  }>
+): JSX.Element;
+export declare function useSingleton<S extends Singleton>(
+  singletonClass: Constructor<S>
+): S;
 export {};
